100abedd75d54e8427958ab46fc47c2ac3739124
Luke Hedge <hedge.luke@gmail.com>
2014-03-01 17:02:30 +1100

    Started dataframes

--- analysis.R
+++ analysis.R
@@ -198,6 +198,66 @@ image(volcano)
 #########
 #SUMMARY STATISTICS
 ########
+limbs <- c(4, 3, 4, 3, 2, 4, 4, 4)
+names(limbs) <- c('One-Eye', 'Peg-Leg', 'Smitty', 'Hook', 'Scooter', 'Dan', 'Mikey', 'Blackbeard')
+
+mean(limbs)
+
+barplot(limbs)
+abline(h = mean(limbs))
+
+limbs <- c(4, 3, 4, 3, 2, 4, 4, 14)
+names(limbs) <- c('One-Eye', 'Peg-Leg', 'Smitty', 'Hook', 
+                    'Scooter', 'Dan', 'Mikey', 'Davy Jones')
+mean(limbs)
+median(lim­bs) 
+
+abline(h = median(limbs))
+
+pounds <- c(45000, 50000, 35000, 40000, 35000, 45000, 10000, 15000)
+barplot(pounds)
+meanvalue<-mean(pounds)
+abline(h=meanvalue)
+
+deviation<-sd(pounds)
+abline(h = meanvalue + deviation)
+
+
+#######
+#FACTORS
+########
+chests <- c('gold', 'silver', 'gems', 'gold', 'gems')
+types <- factor(chests)
+print(chests)
+print(types)
+
+as.integer(types)
+
+levels(types)
+weights <- c(300, 200, 100, 250, 150)
+prices <- c(9000, 5000, 12000, 7500, 18000)
+plot(weights, prices)
+
+plot(weights, prices, pch=as.integer(types))
+legend("topright", c("gems", "gold", "silver"), pch=1:3)
+legend("topright", levels(types), pch=1:length(levels(types)))
+
+
+#########
+#DATA FRAME
+########
+
+#combine vectors
+treasure <- data.frame(weights, prices, types)
+treasure
+
+#accessing by indice brackets
+treasure[[2]]
+treasure[["weights"]]
+
+#shorthand
+treasure$prices
+treasure$types
 
 
 
