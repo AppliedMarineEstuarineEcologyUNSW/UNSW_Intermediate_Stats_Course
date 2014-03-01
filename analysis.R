2369adea183549a89079fd71438e0a63c610e108
Luke Hedge <hedge.luke@gmail.com>
2014-03-01 15:45:03 +1100

    added Basics, Help, Vectors

--- analysis.R
+++ analysis.R
@@ -1,3 +1,49 @@
 #####
 #UNSW Advanced Statistics Course
 #####
+
+#####################
+#The Basics
+#####################
+
+# Type the string "Arr, matey!". (Don't forget the quotes!)
+
+#Now try multiplying 6 times 7
+
+3 < 4
+
+2 + 2 == 5
+
+T == TRUE
+
+x <- 42
+
+x / 2
+
+x <- "Arr, matey!"
+x
+
+x <- TRUE
+
+################
+#FUNCTIONS
+################
+
+sum(1, 3, 5)
+
+rep("Yo ho!", times = 3)
+
+sqrt(16)
+
+##########
+#HElP
+#########
+help(sum)
+
+example(min)
+
+#####
+#VECTORS
+#####
+
+c(4, 7, 9) 
