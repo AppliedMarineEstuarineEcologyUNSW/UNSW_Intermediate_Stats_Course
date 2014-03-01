5ac8e7bbbbd68e7badabeb5467ed39b42420db42
Luke Hedge <hedge.luke@gmail.com>
2014-03-01 16:06:53 +1100

    Added scatterplots and vector maths

--- analysis.R
+++ analysis.R
@@ -47,3 +47,101 @@ example(min)
 #####
 
 c(4, 7, 9) 
+
+c('a', 'b', 'c') 
+
+###VECTORS Cannot hold multiple types of DATA
+c(1, TRUE, "three")
+#Note that the vector has converted all into CHARACTER STRINGS
+
+5:9
+
+seq(5, 9) 
+
+seq(5, 9, 0.5)
+
+9:5 
+
+######
+#Accessing VECTORS
+######
+
+sentence <- c('walk', 'the', 'plank')
+sentence[3]
+
+sentence[1]
+
+#assign elements of a vector 
+sentence[3­] <- "dog"­
+sentence[4] <- 'to'
+
+#accessing multiple values
+sentence[c(1, 3)]
+
+sentence[2:4]
+
+#assigning multiple values and growing a vector
+sentence[5:7] <- c('the', 'poop', 'deck')
+
+#########
+#VECTOR NAMES
+########
+ranks <- 1:3
+names(ranks) <- c("first", "second", "third")
+ranks["first"]
+
+#######
+#PLOTTING A VECTOR
+######
+
+barplot(1:100)
+
+vesselsSunk <- c(4, 5, 1)
+barplot(vesselsSunk)
+
+names(vesselsSunk) <- c("England", "France", "Norway")
+barplot(vesselsSunk)
+
+
+#######
+#VECTOR MATH
+######
+
+a <- c(1, 2, 3)
+a + 1
+
+a / 2 
+
+a*2
+
+b <- c(4, 5, 6)
+a + b
+
+a-b
+
+a == c(1, 99, 3)
+
+sin(a)
+
+sqrt(a)
+
+######
+#Scatter Plots
+######
+
+#Plot One
+x <- seq(1, 20, 0.1)
+y <- sin(x)
+
+#Plot Two
+values <- -10:10
+absolutes <- abs(values)
+plot(values, absolutes)
+
+
+########
+#NA's
+#######
+
+
+
